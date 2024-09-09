f11f59ca7155083796b1baf131a6ab51f9d51ff3
Ashu-Acuver <ashutosh.panigrahi@acuverconsulting.com>
2024-09-09 17:30:37 +0530

    initial commit

---
+++ src/main/java/com/tracingSystem/DemoTracingService/DemoTracingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tracingSystem.DemoTracingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTracingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTracingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracingSystem/DemoTracingService/DemoTracingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracingSystem.DemoTracingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTracingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
